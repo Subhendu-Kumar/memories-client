2375fc3e8e6a485290fe6462870703b417c91c5c
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-01-27 20:22:25 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const NEXT_PUBLIC_URL = 'https://memories-backend-subhendu.vercel.app'
-export const NEXT_PUBLIC_URL = "http://localhost:5000";
+export const NEXT_PUBLIC_URL = 'https://memories-backend-subhendu.vercel.app'
+// export const NEXT_PUBLIC_URL = "http://localhost:5000";
